a62389402ddef21ae6cc78370cbbe89dd3f4a816
21cookej <21cookej@roundwoodpark.co.uk>
2024-12-05 10:50:37 +0000

    Update infinite.js

--- infinite/infinite.js
+++ infinite/infinite.js
@@ -1,4 +1,4 @@
 // javascript:alert("Ready? IF NO CLICK OK.");setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000);
 // javascript:setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000000);
-javascript:open('https://discord.com/app?'+'i'.repeat(16380))
-javascript:onbeforeunload=i=>1
+//javascript:open('https://discord.com/app?'+'i'.repeat(16380))
+//javascript:onbeforeunload=i=>1
